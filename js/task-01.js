9ddd0be902a83517fb8a4e9762f8c1ff1b41face
xdpiqbx <xdpiqbx@gmail.com>
2020-09-12 00:18:19 +0300

    task 01  redy

--- js/task-01.js
+++ js/task-01.js
@@ -3,14 +3,21 @@
 
 Посчитает и выведет в консоль количество категорий в ul#categories,
 то есть элементов li.item. Получится 'В списке 3 категории.'.
+*/
+const categoriesList = document.querySelector("#categories");
+const items = categoriesList.querySelectorAll(".item");
+console.log(`В списке ${items.length} категории`);
 
+/*
 Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль
 текст заголовка элемента (тега h2) и количество элементов в категории (всех вложенных в него элементов li).
-
 Например для первой категории получится:
 Категория: Животные
 Количество элементов: 4
 */
-
-const categoriesList = document.getElementById("categories");
-console.log(categoriesList);
+for (let item of items){
+    console.log(`Категория: ${item.querySelector('h2').innerText}`)
+    for (let li of item.querySelectorAll("ul")){
+        console.log(`Количество элементов: ${li.querySelectorAll("li").length}`)
+    }
+}
---
+++ js/task-02.js
@@ -0,0 +1,2 @@
+//https://www.youtube.com/watch?v=-qfbP5mM9MM&feature=youtu.be
+//https://github.com/goitacademy/javascript-homework/tree/master/homework-07
