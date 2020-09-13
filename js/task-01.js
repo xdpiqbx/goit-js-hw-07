e1d570afc72f6866f69f31fb2a39884048cf06dc
xdpiqbx <xdpiqbx@gmail.com>
2020-09-13 13:43:20 +0300

    Добавляет всю домашку 8 + адаптивка)

--- js/task-01.js
+++ js/task-01.js
@@ -4,7 +4,6 @@
 Посчитает и выведет в консоль количество категорий в ul#categories,
 то есть элементов li.item. Получится 'В списке 3 категории.'.
 */
-
 const categoriesList = document.querySelector("#categories");
 const items = categoriesList.querySelectorAll(".item");
 console.log(`В списке ${items.length} категории`);
@@ -16,10 +15,32 @@ console.log(`В списке ${items.length} категории`);
 Категория: Животные
 Количество элементов: 4
 */
-
 for (let item of items){
     console.log(`Категория: ${item.querySelector('h2').innerText}`)
     for (let li of item.querySelectorAll("ul")){
         console.log(`Количество элементов: ${li.querySelectorAll("li").length}`)
     }
 }
+
+
+/* Вывод в браузер ============================================================ */
+const consoleDiv = document.querySelector("#console")
+
+const subTask1 = document.createElement("div")
+subTask1.classList.add("sub-task-1")
+
+const subTask2 = document.createElement("div")
+subTask2.classList.add("sub-task-2")
+
+subTask1.insertAdjacentHTML('beforeend', `<pre>В списке ${items.length} категории</pre>`)
+
+for (let item of items){
+    let text = `<pre>Категория: ${item.querySelector('h2').innerText}<br>`
+    for (let li of item.querySelectorAll("ul")){
+        text += `Количество элементов: ${li.querySelectorAll("li").length}</pre>`
+    }
+    subTask2.insertAdjacentHTML('beforeend', text)
+}
+
+consoleDiv.insertAdjacentElement("beforeend", subTask1)
+consoleDiv.insertAdjacentElement("beforeend", subTask2)
